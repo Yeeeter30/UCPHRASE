615959adc62aadecb7ede4ddcc8768b69704b644
PirxcyFinal <120330677+Yeeeter30@users.noreply.github.com>
2025-01-02 01:04:17 -0500

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,14 @@
+{
+    "name": "UCPHRASE 1.0",
+    "author": "@nivalos",
+    "tabs": [
+        {
+            "Name": "Words",
+            "Icon": "book-a",
+            "Content": [
+                "nịg̣g̣а",
+                "Phrase #2"
+            ]
+        }
+    ]
+}
