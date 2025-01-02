ea9f98e6eca0d5d745aee38daa691beee06388f0
PirxcyFinal <120330677+Yeeeter30@users.noreply.github.com>
2025-01-02 01:29:32 -0500

    Add files via upload

---
+++ ucphrase.lua
@@ -0,0 +1,18 @@
+{
+    "name": "UCPHRASE 1.0",
+    "author": "@nivalos",
+    "tabs": [
+        {
+            "Name": "WordsV2",
+            "Icon": "book-a",
+            "Content": [
+                "nịg̣g̣а",
+                "nịg̣g̣ẹṛ",
+                "nịg̣g̣аṣ",
+                "nịg̣g̣ẹṛṣ",
+                "f̣аg̣g̣ọṭ",
+                "f̣аg̣g̣ọṭṣ"
+            ]
+        }
+    ]
+}
